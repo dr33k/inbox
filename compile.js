ee461e9f8fa06b90443b8a95d921835a27b7278d
MichaelDreek <emeliec@hardcorebiometric.com>
2024-12-22 11:19:01 +0100

    Initial commit for Inbox contract

---
+++ compile.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+var input = {
+    language: 'Solidity',
+    sources: {
+        'inbox.sol' : {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': [ '*' ]
+            }
+        }
+    }
+};
+
+const compileres = JSON.parse(solc.compile(JSON.stringify(input)));
+
+console.log(compileres);
+module.exports = compileres.contracts['inbox.sol']
+
